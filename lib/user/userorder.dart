ea9d6ffd6de23e66f6c603994b21264c5547180e
Setthap0ng <setthapongkk3@gmail.com>
2023-09-05 23:36:49 +0700

    Updated.

--- lib/user/userorder.dart
+++ lib/user/userorder.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, unnecessary_string_interpolations
-
 import 'package:finalproject_t_shop/models/users.dart';
 import 'package:finalproject_t_shop/screens/sidemenu.dart';
 import 'package:finalproject_t_shop/user/userorderdetail.dart';
@@ -15,63 +13,66 @@ class UserOrder extends StatelessWidget {
     var user = ModalRoute.of(context)!.settings.arguments as Users;
 
     return Scaffold(
-        appBar: AppBar(
-            title: const Text("My Order"), backgroundColor: Color(0xFF2E2E2E)),
-        drawer: SideMenu(),
-        body: ListView.builder(
-          padding: const EdgeInsets.all(10),
-          itemCount: user.myorder!.length,
-          itemBuilder: (context, index) {
-            var imgorder = user.myorder![index].img;
-            imgorder ??=
-                'https://t3.ftcdn.net/jpg/04/62/93/66/360_F_462936689_BpEEcxfgMuYPfTaIAOC1tCDurmsno7Sp.jpg';
-            return Card(
-              child: Column(
-                children: [
-                  ListTile(
-                    title: Text(
-                      "#${user.myorder?[index].id} - ${user.myorder?[index].name ?? 'N/A'}",
-                    ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(height: 5),
-                        Text(
-                            "Price: ${user.myorder?[index].price != null ? '${user.myorder?[index].price} THB' : 'N/A'}"),
-                        SizedBox(height: 5),
-                        Text("Size: ${user.myorder?[index].size ?? 'N/A'}"),
-                        SizedBox(height: 5),
-                        Text("Count: ${user.myorder?[index].count ?? 'N/A'}"),
-                        SizedBox(height: 5),
-                        Text(
-                            "Total Price: ${user.myorder?[index].totalprice != null ? '${user.myorder?[index].totalprice} THB' : 'N/A'}"),
-                        SizedBox(height: 5),
-                      ],
-                    ),
-                    leading: AspectRatio(
-                      aspectRatio: 1,
-                      child: Image.network(imgorder),
-                    ),
+      appBar: AppBar(
+        title: const Text("My Order"),
+        backgroundColor: const Color(0xFF2E2E2E),
+      ),
+      drawer: SideMenu(),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: user.myorder!.length,
+        itemBuilder: (context, index) {
+          var imgorder = user.myorder![index].img;
+          imgorder ??=
+              'https://t3.ftcdn.net/jpg/04/62/93/66/360_F_462936689_BpEEcxfgMuYPfTaIAOC1tCDurmsno7Sp.jpg';
+          return Card(
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text(
+                      "#${user.myorder?[index].id} - ${user.myorder?[index].name ?? 'N/A'}"),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 5),
+                      Text(
+                          "Price: ${user.myorder?[index].price != null ? '${user.myorder?[index].price} THB' : 'N/A'}"),
+                      const SizedBox(height: 5),
+                      Text("Size: ${user.myorder?[index].size ?? 'N/A'}"),
+                      const SizedBox(height: 5),
+                      Text("Count: ${user.myorder?[index].count ?? 'N/A'}"),
+                      const SizedBox(height: 5),
+                      Text(
+                          "Total Price: ${user.myorder?[0].totalprice != null ? '${user.myorder?[index].totalprice} THB' : 'N/A'}"),
+                      const SizedBox(height: 5),
+                    ],
                   ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => UserOrderDetail(),
-                          settings: RouteSettings(arguments: user),
-                        ),
-                      );
-                    },
-                    child: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Text("View Order Detail",
-                            style: TextStyle(color: Colors.blue))),
+                  leading: AspectRatio(
+                    aspectRatio: 1,
+                    child: Image.network(imgorder),
                   ),
-                ],
-              ),
-            );
-          },
-        ));
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            UserOrderDetail(myOrder: user.myorder![index]),
+                      ),
+                    );
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.all(12.0),
+                    child: Text("View Order Detail",
+                        style: TextStyle(color: Colors.blue)),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
--- lib/user/userorderdetail.dart
+++ lib/user/userorderdetail.dart
@@ -1,61 +1,73 @@
-// ignore_for_file: prefer_const_constructors, no_leading_underscores_for_local_identifiers
+// ignore_for_file: prefer_const_constructors
 
-import 'package:finalproject_t_shop/models/users.dart';
+import 'package:finalproject_t_shop/models/config.dart';
 import 'package:flutter/material.dart';
+import 'package:finalproject_t_shop/models/users.dart';
 
 class UserOrderDetail extends StatelessWidget {
-  const UserOrderDetail({super.key});
+  final Myorder myOrder;
+  Users user = Configure.login;
+
+  UserOrderDetail({Key? key, required this.myOrder});
 
   @override
   Widget build(BuildContext context) {
-    final _formKey = GlobalKey<FormState>();
-    var user = ModalRoute.of(context)!.settings.arguments as Users;
-    var imgorder = user.myorder![0].img;
-    imgorder ??=
+    // ใช้ข้อมูลจาก myOrder ในการแสดงข้อมูลที่ถูกเลือก
+    final imgOrder = myOrder.img ??
         'https://icon-library.com/images/no-picture-available-icon/no-picture-available-icon-20.jpg';
+
     return Scaffold(
-        appBar: AppBar(
-            title: Text("T-Shirt Detail"), backgroundColor: Color(0xFF2E2E2E)),
-        body: Container(
-          margin: const EdgeInsets.all(10),
-          child: Form(
-            key: _formKey,
-            child: Card(
-              child: ListView(
-                children: [
-                  AspectRatio(
-                      aspectRatio: 16 / 9, child: Image.network(imgorder)),
-                  ListTile(
-                      title:
-                          Text("T-Shirt Name", style: TextStyle(fontSize: 15)),
-                      subtitle: Text("${user.myorder?[0].name}")),
-                  ListTile(
-                      title: Text("T-Shirt Price"),
-                      subtitle: Text("${user.myorder?[0].price} THB")),
-                  ListTile(
-                      title: Text("T-Shirt Size"),
-                      subtitle: Text("${user.myorder?[0].size}")),
-                  ListTile(
-                      title: Text("T-Shirt Count"),
-                      subtitle: Text("${user.myorder?[0].count}")),
-                  ListTile(
-                      title: Text("Total Price"),
-                      subtitle: Text("${user.myorder?[0].totalprice} THB")),
-                  ListTile(
-                      title: Text("Firstname"),
-                      subtitle: Text("${user.firstname}")),
-                  ListTile(
-                      title: Text("Lastname"),
-                      subtitle: Text("${user.lastname}")),
-                  ListTile(
-                      title: Text("Phone"), subtitle: Text("${user.phone}")),
-                  ListTile(
-                      title: Text("Address"),
-                      subtitle: Text("${user.address}")),
-                ],
-              ),
+      appBar: AppBar(
+        title: Text("T-Shirt Detail"),
+        backgroundColor: Color(0xFF2E2E2E),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Image.network(imgOrder),
             ),
-          ),
-        ));
+            ListTile(
+              title: Text("T-Shirt Name", style: TextStyle(fontSize: 15)),
+              subtitle: Text("${myOrder.name}"),
+            ),
+            ListTile(
+              title: Text("T-Shirt Price"),
+              subtitle: Text("${myOrder.price} THB"),
+            ),
+            ListTile(
+              title: Text("T-Shirt Size"),
+              subtitle: Text("${myOrder.size}"),
+            ),
+            ListTile(
+              title: Text("T-Shirt Count"),
+              subtitle: Text("${myOrder.count}"),
+            ),
+            ListTile(
+              title: Text("Total Price"),
+              subtitle: Text("${myOrder.totalprice} THB"),
+            ),
+            ListTile(
+              title: Text("Firstname"),
+              subtitle: Text("${user.firstname}"),
+            ),
+            ListTile(
+              title: Text("Lastname"),
+              subtitle: Text("${user.lastname}"),
+            ),
+            ListTile(
+              title: Text("Phone Number"),
+              subtitle: Text("${user.phone}"),
+            ),
+            ListTile(
+              title: Text("Address"),
+              subtitle: Text("${user.address}"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
