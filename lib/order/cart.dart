c9cdee12ffef37c79261bc933af86de2adbaec54
Setthap0ng <setthapongkk3@gmail.com>
2023-09-09 14:36:17 +0700

    Updated.

--- lib/order/cart.dart
+++ lib/order/cart.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, unused_local_variable, avoid_print
+// ignore_for_file: prefer_const_constructors, unused_local_variable, avoid_print, camel_case_types
 
 import 'dart:convert';
 
