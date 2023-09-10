de02cb6fb9d63bce2160bd4ac770af1048f7d726
Setthap0ng <setthapongkk3@gmail.com>
2023-09-10 14:25:43 +0700

    Updated.

--- lib/models/config.dart
+++ lib/models/config.dart
@@ -2,7 +2,7 @@ import 'package:finalproject_t_shop/models/tshirt.dart';
 import 'package:finalproject_t_shop/models/users.dart';
 
 class Configure {
-  static const server = "10.161.2.51:3000";
+  static const server = "10.112.6.38:3000";
   static Users login = Users();
   static Tshirt ts = Tshirt();
   static List<String> size = ["S", "M", "L", "XL"];
